0f23718cdfb9b3e2588a26832760dac6f5160b9b
Uri Kutner <urikutner@gmail.com>
2020-03-24 13:32:07 +0200

    replace runtime tabs with inline tabs

--- components/template/vue/snapshots/vue-default-code.ts
+++ components/template/vue/snapshots/vue-default-code.ts
@@ -1,26 +1,24 @@
-const snapshot = `<template>
-  <CodeGenerator />
+export default `<template>
+	<CodeGenerator />
 </template>
 
 <script>
-  import CodeGenerator from '@bit/bit.javascript.raw.code-generator';
-  
-  export default (
-    {
-      data () {
-        return {
-          var1: 'World'
-        }
-      },
-      components: {
-        CodeGenerator
-      }
-    }
-  )
+	import CodeGenerator from '@bit/bit.javascript.raw.code-generator';
+	
+	export default (
+		{
+			data () {
+				return {
+					var1: 'World'
+				}
+			},
+			components: {
+				CodeGenerator
+			}
+		}
+	)
 </script>
 
 <style scoped>
 
-</style>`
-
-export default snapshot.replace(/  /g, '\t')
+</style>`;
